ce7ed315bbaf64df0089f684bd5956d8f2bd0547
giftlg <77376272+giftlg@users.noreply.github.com>
2021-11-23 20:07:29 +0200

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/adolescentonlinesafety/MainActivity.java
+++ app/src/main/java/com/example/adolescentonlinesafety/MainActivity.java
@@ -32,7 +32,8 @@ import io.paperdb.Paper;
 public class MainActivity extends AppCompatActivity {
 
 
-   public Button login_btn,create_account;
+    public Button login_btn;
+    TextView create_account;
     public static EditText input_email,input_password;
     private ProgressDialog loadingbar;
     public static String email_name;
@@ -53,48 +54,18 @@ public class MainActivity extends AppCompatActivity {
         input_email =  findViewById(R.id.login_email);
         input_password = findViewById(R.id.login_password);
         loadingbar= new ProgressDialog(this);
+
         create_account = findViewById(R.id.Create_account);
         loadinbar=new ProgressDialog(this);
+
         Paper.init(this);
-
-
-
-
-
-
-
-        create_account.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view)
-          {
-              Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
-              startActivity(intent);
-
-          }
-      });
-
-
-
-//action to be performed when login button is clicked
-
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                //login method
-                LoginUser();
-            }
-        });
-
 
         String UserEmailKey = Paper.book().read(Prevalent.UserEmailKey);
         String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
 
-        if (UserEmailKey !="" && UserPasswordKey !="")
-        {
-            if (!TextUtils.isEmpty(UserEmailKey) &&  !TextUtils.isEmpty(UserPasswordKey))
-            {
-                AlloweAccess(UserEmailKey,UserPasswordKey);
+        if (UserEmailKey !="" && UserPasswordKey !="") {
+            if (!TextUtils.isEmpty(UserEmailKey) && !TextUtils.isEmpty(UserPasswordKey)) {
+                AlloweAccess(UserEmailKey, UserPasswordKey);
 
                 loadinbar.setTitle("");
                 loadinbar.setMessage("Please wait....");
@@ -103,17 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-
-
-
-
-
-
     }
-
     private void AlloweAccess(final String login_email, final String login_password)
 
     {
+
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
 
@@ -157,12 +122,50 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+//action to be performed when login button is clicked
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                //login method
+                LoginUser();
+            }
+        });
+
+
+
+
+
     }
+
+
+
+
+
     // login method to be called
     private void LoginUser()
     {
         String login_email = input_email.getText().toString().replace('.',',') ;;
-        String login_password = input_password.getText().toString();
+        String login_password = input_password.getText().toString().replace('a','x'); //manual login string covet
 
 
         if (TextUtils.isEmpty(login_email))
@@ -189,11 +192,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void AllowAccessToAccount(String login_email, String login_password)
     {
-        if (checkbox_rememberMe.isChecked())
-        {
-            Paper.book().write(Prevalent.UserEmailKey,login_email);
-            Paper.book().write(Prevalent.UserPasswordKey,login_password);
-        }
+
+
+        Paper.book().write(Prevalent.UserEmailKey,login_email);
+        Paper.book().write(Prevalent.UserPasswordKey,login_password);
+
 
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
@@ -262,7 +265,69 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     }
+    // inflating menu on the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //on click menu item action
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.share:
+                try {
+
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Adolescent Online Safety app");
+                    String shareMessage= "\n Let me recommend you this application \n https://drive.google.com/file/d/1iaDsnZ3GOTHx0DWy8wscdHrT_cBaJJhp/view?usp=sharing\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+
+
+                return true;
+            case R.id.feedback:
+                Toast.makeText(this, "Give Feedback", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+
+            case R.id.about:
+
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog_layout);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                dialog.show();
+
+                //setting animation of dialog
+                dialog.getWindow().setWindowAnimations(R.style.AnimationsForDialog);
+
+
+                return true;
+
+
+            case R.id.exit:
+
+                finish();
+
+            default:
+
+
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 
 
 }
